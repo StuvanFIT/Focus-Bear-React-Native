9511ffd58e4da0d49478133c284a38c53bcc16f4
skai0008 <kaingsteven123@gmail.com>
2025-11-13 03:51:52 +1100

    refactor orderUtils

--- milestones/4_Clean_Code/orderUtils.java
+++ milestones/4_Clean_Code/orderUtils.java
@@ -1,12 +1,27 @@
 public double calculateOrder(double[] prices, double discountRate, double taxRate) {
+    double subtotal = calculateSubtotal(prices);
+    double discounted = applyDiscount(subtotal, discountRate);
+    double total = applyTax(discounted, taxRate);
+    printSummary(subtotal, discounted, total);
+    return total;
+}
+
+private double calculateSubtotal(double[] prices) {
     double subtotal = 0;
-    for (double price : prices) {
-        subtotal += price;
-    }
-    double discount = subtotal * discountRate;
-    double taxedTotal = (subtotal - discount) + ((subtotal - discount) * taxRate);
+    for (double price : prices) subtotal += price;
+    return subtotal;
+}
+
+private double applyDiscount(double subtotal, double discountRate) {
+    return subtotal - (subtotal * discountRate);
+}
+
+private double applyTax(double amount, double taxRate) {
+    return amount + (amount * taxRate);
+}
+
+private void printSummary(double subtotal, double discounted, double total) {
     System.out.println("Subtotal: " + subtotal);
-    System.out.println("Discount: " + discount);
-    System.out.println("Taxed Total: " + taxedTotal);
-    return taxedTotal;
+    System.out.println("After Discount: " + discounted);
+    System.out.println("Final Total: " + total);
 }
